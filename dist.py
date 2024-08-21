475239cd8ac4cf6b8b567b8e02ac870361ec2c3a
MBUYt0n <shusrith03@gmail.com>
2024-08-21 05:37:30 +0530

    i think we're done

--- dist.py
+++ dist.py
@@ -5,7 +5,8 @@ import logging
 logging.getLogger("ultralytics").setLevel(logging.ERROR)
 
 
-def org(res, curr):
+def org(res):
+    curr = {}
     for i in res:
         if i[0] not in curr:
             curr[i[0]] = []
@@ -50,12 +51,11 @@ def movement(prev, curr, frame):
             for objp, objc in zip(prev, curr):
                 curr_area = objc["area"]
                 prev_area = objp["area"]
-                print(curr_area, prev_area)
                 if curr_area < prev_area:
                     label = "Moving Away"
                 else:
                     label = "Moving Towards"
-
+                print(i, objc["mid"], label)
                 x1, y1, x2, y2 = (
                     objc["x1"],
                     objc["y1"],
@@ -76,6 +76,15 @@ def movement(prev, curr, frame):
 
 yolo = YOLO("yolov10x.pt")
 cap = cv2.VideoCapture("/home/shusrith/vids/l4.mp4")
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+out = cv2.VideoWriter(
+    "output.avi",
+    cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+    10,
+    (frame_width, frame_height),
+)
+
 frame_count = 0
 prev, curr = {}, {}
 while True:
@@ -89,31 +98,12 @@ while True:
     results = yolo(frame)
     res = [(i["name"], i["box"]) for j in results for i in j.summary()]
     prev = curr
-    curr = org(res, curr)
+    curr = org(res)
     if prev is not None:
         movement(prev, curr, frame)
 
-    # for match in res:
-    #     cv2.rectangle(
-    #         frame,
-    #         (int(match[1]["x1"]), int(match[1]["y1"])),
-    #         (int(match[1]["x2"]), int(match[1]["y2"])),
-    #         (0, 255, 0),
-    #         2,
-    #     )
-    #     cv2.putText(
-    #         frame,
-    #         f"{match[0]}",
-    #         (int(match[1]["x1"]), int(match[1]["y1"])),
-    #         cv2.FONT_HERSHEY_SIMPLEX,
-    #         0.5,
-    #         (0, 255, 0),
-    #         2,
-    #     )
-    curr = {}
-    cv2.imshow("frame1", frame)
-    if cv2.waitKey(1000) & 0xFF == ord("q"):
-        break
+    out.write(frame)
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
